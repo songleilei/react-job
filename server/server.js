b220b0c21ce9dab105ab9018aec6a14dfbc1a92f
rf y <709241197@qq.com>
2019-12-08 01:15:49 +0800

    1、装饰器插件更换为 @babel/plugin-proposal-decorators； 2、html-webpack-plugin 使用 next 版本，更改 webpack 配置中的用法； 3、autoprefixer 插件使用最新用法； 4、mongoose db 更换为最新用法； 5、window.devToolsExtension 更换redux调试的api； 6、头像选择组件使用import，导出所有头像，之前的require会有not found问题；

--- server/server.js
+++ server/server.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const model = require('./model');
-const Chat = model.getModel('chat');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const model = require('./model')
+const Chat = model.getModel('chat')
 
-// const path = require('path'); 
+// const path = require('path');
 // 新建app
-const app = express();
+const app = express()
 
 //设置跨域访问
-app.all('*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By", ' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
-});
+app.all('*', function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+	res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+	res.header('X-Powered-By', ' 3.2.1')
+	res.header('Content-Type', 'application/json;charset=utf-8')
+	next()
+})
 
 // 将http与socket统一起来
-const server = require('http').Server(app);
+const server = require('http').Server(app)
 
-const io = require('socket.io')(server);
+const io = require('socket.io')(server)
 
 // Chat.remove({
 // }, function (err, doc) {
@@ -32,31 +32,34 @@ const io = require('socket.io')(server);
 //     }
 // })
 
-io.on('connection', function (socket) {
-    console.log('user connect');
-    socket.on('sendmsg', function (data) {
-        const { from, to, msg } = data;
-        const chatid = [from, to].sort().join('_')
-        Chat.create({ chatid, from, to, content: msg, creat_time: new Date().getTime()}, function(err, doc){
-            if (!err) {
-                io.emit('recvmsg', Object.assign({},doc._doc))
-            }
-        })
-    })
+io.on('connection', function(socket) {
+	console.log('user connect')
+	socket.on('sendmsg', function(data) {
+		const { from, to, msg } = data
+		const chatid = [from, to].sort().join('_')
+		Chat.create(
+			{ chatid, from, to, content: msg, creat_time: new Date().getTime() },
+			function(err, doc) {
+				if (!err) {
+					io.emit('recvmsg', Object.assign({}, doc._doc))
+				}
+			}
+		)
+	})
 })
 
-const userRouter = require('./user');
+const userRouter = require('./user')
 
-app.use(cookieParser());
+app.use(cookieParser())
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.use('/user', userRouter);
+app.use('/user', userRouter)
 
 // 编译打包后
-// 购买域名 
-// DNS解析到服务器的ip 
-// 安装nginx 
+// 购买域名
+// DNS解析到服务器的ip
+// 安装nginx
 // 使用pm2 管理node进程
 
 /**
@@ -69,6 +72,10 @@ app.use('/user', userRouter);
     app.use('/',express.static(path.resolve('build')))
  */
 
-server.listen(9093, function () {
-    console.log('Node app start at port 9093');
-});
+// app.get("/", function(req, res) {
+//   res.send("<h1>Hello world 示例!!!</h1>");
+// });
+
+server.listen(9093, function() {
+	console.log('Node app start at port 9093')
+})
--- src/component/avatar-selector/avatar-selector.js
+++ src/component/avatar-selector/avatar-selector.js
@@ -1,48 +1,59 @@
-import React from 'react';
-import { Grid, List } from "antd-mobile";
-import PropTypes from 'prop-types';
+import React from 'react'
+import { Grid, List } from 'antd-mobile'
+import PropTypes from 'prop-types'
 
 class AvatarSelector extends React.Component {
+	static propTypes = {
+		selectAvatar: PropTypes.func.isRequired
+	}
 
-    static propTypes = {
-        selectAvatar: PropTypes.func.isRequired
-    }
+	constructor(props) {
+		super(props)
+		this.state = {}
+	}
 
-    constructor(props){
-        super(props)
-        this.state={
+	render() {
+		const avatarList = Array.from(new Array(20)).map((_val, i) => ({
+			// icon: require(`../../avatar/${i + 1}.png`)
+			icon: images[`${i + 1}.png`].default,
+			index: `${i + 1}`
+		}))
 
-        }
-    }
+		const gridHeader = this.state.icon ? (
+			<div>
+				<span>已选择头像</span>
+				<img style={{ width: 20 }} src={this.state.icon} alt="" />
+			</div>
+		) : (
+			<div>请选择头像</div>
+		)
 
-    render() {
-        const avatarList = Array.from(new Array(20)).map((_val, i) => ({
-            icon: require(`../../avatar/${i+1}.png`),
-            index: `${i+1}`,
-          }));
-
-        const gridHeader = this.state.icon ? 
-                            (<div>
-                                <span>已选择头像</span>
-                                <img style={{width:20}} src={this.state.icon} alt=""/>
-                            </div>)
-                            :<div>请选择头像</div>
-
-        return (
-            <div>
-                <List renderHeader={()=>gridHeader}>
-                    <Grid 
-                        data={avatarList} 
-                        columnNum={5} 
-                        onClick={ele=>{
-                            this.setState(ele)
-                            this.props.selectAvatar(ele.index)
-                        }}
-                    />
-                </List>
-            </div>
-        )
-    }
+		return (
+			<div>
+				<List renderHeader={() => gridHeader}>
+					<Grid
+						data={avatarList}
+						columnNum={5}
+						onClick={(ele) => {
+							this.setState(ele)
+							this.props.selectAvatar(ele.index)
+						}}
+					/>
+				</List>
+			</div>
+		)
+	}
 }
+
+const importAll = (r) => {
+	let images = {}
+	r.keys().map((item, index) => {
+		images[item.replace('./', '')] = r(item)
+	})
+
+	return images
+}
+
+const images = importAll(require.context('../../avatar', false, /.png$/))
 
 export default AvatarSelector
